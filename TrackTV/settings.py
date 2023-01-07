19c6b13e4ea7859a07c66afe9d7cd8de7bbc104a
sgrsaga <sgrsagara@gmail.com>
2023-01-08 00:05:05 +0530

    commected setting.py #SECRET_KEY

--- TrackTV/settings.py
+++ TrackTV/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rj+6mpu89^cp_eu5-&0it+)9ss7)r2*8-9di&0j)te_&zpuz)3'
+#SECRET_KEY = 'rj+6mpu89^cp_eu5-&0it+)9ss7)r2*8-9di&0j)te_&zpuz)3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
